62693f36e7f1ca765bff711a49dec0200f1003c8
luoxinran28 <luoxinran28@gmail.com>
2016-02-03 17:07:24 -0700

    revert to previous one

--- modules/categories/client/categories.client.module.js
+++
@@ -1,5 +0,0 @@
-// Use application configuration module to register a new module
-(function (app) {
-  'use strict';
-  app.registerModule('categories');
-})(ApplicationConfiguration);
---
+++ modules/categories/server/controllers/categories.server.controller.js
@@ -0,0 +1,96 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+ var mongoose = require('mongoose'),
+ 	 errorHandler = require('./errors.server.controller'),
+	 Category = mongoose.model('Category'),
+	 _ = require('lodash');
+
+/**
+ * Create a Category
+ */
+ exports.create = function (req, res) {
+    var category = new Category(req.body);
+
+    category.save(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.status(201).json(category);
+        }
+    });
+ };
+
+/**
+ * Show the current Category
+ */
+ exports.read = function (req, res) {
+ 	Category.findById()(req.params.categoryId).exec(function(err, category){
+ 		if (err) {
+ 			return res.status.send({
+ 				message: errorHandler.getErrorMessage(err)
+ 			});
+ 		} else{
+ 			if (!category) {
+ 				return res.status(404).send({
+ 					message: 'Category not found'
+ 				});
+ 			}
+ 		}
+ 	});
+ };
+
+/**
+ * Update a Category
+ */
+ exports.update = function (req, res) {
+	var category = req.category;
+
+	category = _.extend(category, req.body);
+
+	category.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(category);
+		}
+	});
+ };
+
+/**
+ * Delete a Category
+ */
+ exports.delete = function (req, res) {
+	var category = req.category;
+
+	category.remove(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(category);
+		}
+	});
+ };
+
+/**
+ * List of Categories
+ */
+ exports.list = function (req, res) {
+ 	Category.find().exec(function(err, categories){
+ 		if (err) {
+ 			return res.status(400).send({
+ 				message: errorHandler.getErrorMessage(err)
+ 			});
+ 		} else{
+ 			res.json(categories);
+ 		}
+ 	});
+ };
